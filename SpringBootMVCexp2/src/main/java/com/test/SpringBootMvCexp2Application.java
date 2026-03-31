d00f65e46e365daee3e36ed8de398b94f0f517f2
Ksobti1414 <kanavsobti@gmail.com>
2026-03-31 12:05:06 +0530

    Add files via upload

---
+++ SpringBootMVCexp2/src/main/java/com/test/SpringBootMvCexp2Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvCexp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvCexp2Application.class, args);
+	}
+
+}
