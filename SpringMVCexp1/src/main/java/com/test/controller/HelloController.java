50eef8fd58211eefb08351df9af03ce8b983b318
Ksobti1414 <kanavsobti@gmail.com>
2026-03-31 12:08:48 +0530

    Add files via upload

---
+++ SpringMVCexp1/src/main/java/com/test/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/Home")  
+    public String homePage() {
+        return "Home";
+    }
+    @GetMapping("/greet")
+    public String greetAgain() {
+    	return "greet";
+    }
+}
