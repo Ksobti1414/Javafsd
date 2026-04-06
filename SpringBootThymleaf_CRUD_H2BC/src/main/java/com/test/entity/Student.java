edc66d3f3f7c32648ea9978a17286976d8585dab
Ksobti1414 <kanavsobti@gmail.com>
2026-04-06 09:26:55 +0530

    Add files via upload

---
+++ SpringBootThymleaf_CRUD_H2BC/src/main/java/com/test/entity/Student.java
@@ -0,0 +1,109 @@
+package com.test.entity;
+
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+
+public class Student {
+
+    
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+
+    
+
+    @NotBlank(message = "Name is mandatory")
+
+    @Column(name = "name")
+
+    private String name;
+
+    
+
+    @NotBlank(message = "Email is mandatory")
+
+    @Column(name = "email")
+
+    private String email;
+    @NotNull(message = "Phone no. is mandatory")
+
+    @Column(name = "phone_no")
+
+    private long phoneNo;
+
+    
+
+    public Student() {}
+
+    public Student(String name, String email) {
+
+        this.name = name;
+
+        this.email = email;
+
+    }
+
+    public void setId(long id) {
+
+        this.id = id;
+
+    }
+
+    
+
+    public long getId() {
+
+        return id;
+
+    }
+
+    
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    
+
+    public void setEmail(String email) {
+
+        this.email = email;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public String getEmail() {
+
+        return email;
+
+    }
+
+	public long getPhoneNo() {
+
+		return phoneNo;
+
+	}
+
+	public void setPhoneNo(long phoneNo) {
+
+		this.phoneNo = phoneNo;
+
+	}
+
+}
+
+
